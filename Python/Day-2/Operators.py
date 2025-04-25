bab69c7fce3f7817920114e958bb003714790ce0
Aditya <adityajoshi316@gmail.com>
2025-04-26 00:36:24 +0200

    some practices

--- Python/Day-2/Operators.py
+++ Python/Day-2/Operators.py
@@ -1,1 +1,38 @@
+#arithmetic operators
+a = 20
+b = 10
 
+print(a+b) 
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+
+
+
+#assign operators
+
+a = 10
+b = 2
+
+a *= b
+print("value of a after assign",a)
+
+
+#comparison operators
+
+a = 10
+b =2
+print(a>b)
+
+
+#logical Operators
+a = 18
+b = 11
+c = 20.2
+
+greater_a_than_b = a > b 
+greater_a_than_c = a > c 
+
+print(greater_a_than_b and greater_a_than_c)
