02caa7a90a009eb8e0adf646867bb8c25a5f2bdc
Aditya <adityajoshi316@gmail.com>
2025-04-27 23:50:17 +0200

    while loop also done

--- Python/Day-3/loops.py
+++ Python/Day-3/loops.py
@@ -10,3 +10,14 @@ for f in fruits:  #f is used to denoate each item in fruits, iterations are done
 
 for i in range(6):
     print(i)
+
+
+#while loop
+
+count = 0
+
+
+while count < 5:
+    print("while loops is running....")
+    print(count)
+    count += 1
