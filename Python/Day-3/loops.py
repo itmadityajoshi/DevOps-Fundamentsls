84d82aa16299b822e38f5d60550033172eff7832
Aditya <adityajoshi316@gmail.com>
2025-04-28 22:16:04 +0200

    control loop

--- Python/Day-3/loops.py
+++ Python/Day-3/loops.py
@@ -28,3 +28,19 @@ for i in range(10):
         if i == 5:
             break
         print(i)  #o/p--> 0,1,2,3,4
+
+
+#continue
+
+for i in range(5):
+    print('continue')
+    if i ==2:
+        continue
+    print(i)
+
+#pass
+
+    for i in range(5):
+        if i ==3:
+            pass
+        print(i)
